c5d9e73c25ae7913b9954675e4704fb24224c398
PrachiShukla2 <prachishuklamount@gmail.com>
2024-01-25 17:07:35 +0530

    js math

---
+++ js math/main.js
@@ -0,0 +1,17 @@
+console.log(Math.PI);
+console.log(Math.trunc(Math.PI));
+console.log(Math.round(Math.PI));
+console.log(Math.ceil(Math.PI));
+// The Math.ceil() static method always rounds up and returns the smallest integer greater than or equal to a given number.
+console.log(Math.floor(Math.PI));
+// The Math.floor() static method always rounds down and returns the largest integer less than or equal to a given number.
+console.log(Math.pow(2,10));
+console.log(Math.pow(2,3));
+// returns the power
+console.log(Math.min(12,56,35));
+console.log(Math.max(12,56,35));
+// returns the smallest number and largest number
+console.log(Math.random());
+// chooose Math.floor() not Math.ceil() for generating a random number from 1 to 10 
+// why?
+// Math.ceil(Math.random()*10)has a chance of resulting in zero.
