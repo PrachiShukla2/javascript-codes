77dc9e6e0404f29b5850ee61943e095f9f61d2cb
PrachiShukla2 <prachishuklamount@gmail.com>
2024-01-25 22:06:41 +0530

    switch

---
+++ js switch/main.js
@@ -0,0 +1,12 @@
+const variable = 45;
+const number = 69;
+switch(100){
+    case number:
+        console.log("matched");
+        break;
+        case variable:
+            console.log("matche"); 
+            break ;
+            default:
+                console.log("not matched");
+}
