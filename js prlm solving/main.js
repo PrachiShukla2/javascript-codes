a062fdde91be22965b3610ce9a75ffedf686b94e
PrachiShukla2 <prachishuklamount@gmail.com>
2024-01-25 17:56:53 +0530

    js math

---
+++ js prlm solving/main.js
@@ -0,0 +1,10 @@
+// .write a code that will return a random letter from your name
+console.log("prachi".charAt(3));
+// generate a random number from 1 to 4
+
+console.log(Math.floor(Math.random()*4 + 1));
+console.log(Math.floor(Math.random()*4 + 1));
+// combine both and solve large prlm
+console.log("prachi".charAt(Math.floor(Math.random()*4 + 1)));
+const anyName = "singapore";
+console.log(anyName.charAt(Math.floor(Math.random()*anyName.length)));
