d2b1e0eed7df9d0bfcbb0eeb152ca00dabadde10
PrachiShukla2 <prachishuklamount@gmail.com>
2024-01-25 18:15:04 +0530

    js method

---
+++ js conditionals/main.js
@@ -0,0 +1,14 @@
+let variable = 56;
+if(variable >10){
+    console.log("it is greater then 10");}
+    else(console.log(" it is smaller"));
+
+    variable = 34;
+    if (variable %2 == 0) {
+        console.log("u won");
+
+    }
+    else if(variable % 34==0){
+        console.log("its equal");
+    }
+    else(console.log(" its very unreal"));
