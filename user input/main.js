568f48145d67e57d5aaf20f53ef759c88449fc80
PrachiShukla2 <prachishuklamount@gmail.com>
2024-01-26 21:29:59 +0530

    userin

---
+++ user input/main.js
@@ -0,0 +1,6 @@
+// let container = prompt("enter a number");
+// // console.log("u entered" + container);
+// alert("Hello world");
+
+let booolean = confirm("ok===true\n cancel === false");
+console.log(booolean);
