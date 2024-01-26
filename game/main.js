ac2db64c3afa61a4e9972ba949b16fb44141aae5
PrachiShukla2 <prachishuklamount@gmail.com>
2024-01-27 00:55:02 +0530

    game code for browser

--- game/main.js
+++ game/main.js
@@ -1,33 +1,49 @@
 
 let game = confirm("will u play");
 if( game){
-    let choice = prompt("please enter \n stone, paper, scissor");
-    if(choice ==="rock"||choice ===" paper"||choice ==="scissor")
-    {
-     let computerChoice =Math.floor(Math.random()*3 +1);
-     let computer = computerChoice === 1
-              ?"rock"
-              :computerChoice === 2
-              ?"paper"
-              :"scisssor";
+    let playerChoice = prompt("please enter \n rock, paper, scissor");
+    if(playerChoice){
+        let playerOne = playerChoice.trim().toLowerCase();
+        if(playerOne ==="rock"||playerOne ==="paper"||playerOne ==="scissor")
+        {
+            let computerChoice = Math.floor(Math.random()*3 +1);
+            let computer = computerChoice === 1
+                     ?"rock"
+                     :computerChoice === 2
+                     ?"paper"
+                     :"scisssor";
+       
+                     let result = 
+                     playerOne === computer
+                     ? "its a draw!"
+                     :playerOne === "rock" && computer === "paper"
+                     ?` playerOne: ${playerOne}\nComputer: ${computer}\ncomputer wins`
+                     :playerOne==="paper" && computer==="scissor"
+                     ?` playerOne: ${playerOne}\nComputer: ${computer}\ncomputer wins `
+                     :playerOne === "scissor" && computer ==="rock"
+                     ?` playerOne: ${playerOne}\nComputer: ${computer}\ncomputer wins`
+                     : ` playerOne: ${playerOne}\nComputer: ${computer}\n playerOne wins`;
+                     alert(result);
+       
+                    let playagain = confirm("play again?"); 
+                    playagain ? location.reload(): alert("thankyou");
+        }else{
+        alert('invalid input please try again');
 
-              let result = 
-              playerOne === computer
-              ? " playerOne: ${playerOne}\nComputer: ${computer}it's a draw!"
-              :playerOne === "rock" && computer === "paper"
-              ?"computer wins"
-              :playerOne==="paper" && computer==="scissor"
-              ?"computer wins "
-              :playerOne === "scissor" && computer ==="rock"
-              ?"computer wins"
-              : " playerOne win!";
-              alert(result);
+    }
+}else{
+    alert("you changed ur mind");
 
-             let playagain = confirm("play again?"); 
+} } 
+else{
+    alert('maybe next tym');
+}
+
+    
+  
              
                
 
-    }
+    
 
 
-}
