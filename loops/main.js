9341695ecef07bbb6cef59e703062ff699ddfbc5
PrachiShukla2 <prachishuklamount@gmail.com>
2024-01-27 21:48:11 +0530

    loop

---
+++ loops/main.js
@@ -0,0 +1,40 @@
+// let container = 50;
+// while(container < 50){
+//     container++;
+//     console.log(container);
+   
+// }
+ 
+
+// // // let factor = 60;
+// do{
+//     console.log(container);
+// }while(container <50);
+
+
+// for(let i =0; i<10; i++)
+// {
+//     console.log(i);
+// }
+
+// let name = "prachi" ;
+// for( let i=0 ;i<= name.length; i++){
+//     console.log(name.charAt(i));
+// }
+
+let name = "prachi" ;
+let counter = 0;
+let letter;
+// // while(true){
+// //     letter =name[counter];
+// //     console.log(letter);
+// //     if(letter === "c")
+// //     break;
+// //     counter++;
+// }
+for(i =0; i= letter =name[counter]; i++){
+    console.log(letter); 
+    if(letter === "c")
+     break;
+     counter++; 
+}
