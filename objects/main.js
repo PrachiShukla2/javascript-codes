7f0c9f75b08800c2ff3050965e105e0315e2b2b7
PrachiShukla2 <prachishuklamount@gmail.com>
2024-01-30 16:34:30 +0530

    objects

---
+++ objects/main.js
@@ -0,0 +1,17 @@
+// objects
+// key value pairs objects are placed in curly braces
+const myobj ={name:"prachi"};
+console.log(myobj);
+console.log(myobj.name);
+
+const anotherObj ={
+    rollno :47,
+    class: "BSCS",
+    hobies :["eat","sleep"],
+     beverages:{
+        morning :"milk",
+        afternoon :"coffee"
+     }
+
+};
+console.log(anotherObj.hobies);
