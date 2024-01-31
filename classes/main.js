18307e3d6ca1599fdee412356ffb929047833ac8
PrachiShukla2 <prachishuklamount@gmail.com>
2024-01-31 16:53:30 +0530

    classes

--- classes/main.js
+++ classes/main.js
@@ -1,14 +1,23 @@
 // classes
 class pizza{
     // In a constructor function this does not have a value. It is a substitute for the new object. The value of this will become the new object when a new object is created.
-    constructor(){
+    constructor(pizzaType){
+        this.type =  pizzaType;
         this.size ="medium";
         this.crust ="original";
     }
+    //  we can access any value by directly using get keyword it acts lyk a method
+    get pizzaCrust(){
+        return this.crust;
+    }
+    // by using set we can directly set the value of our set 
+    set pizzaCrust(pizzaCrust){
+        this.crust = pizzaCrust;
+    }
     bake(){
-        console.log(`baking ${this.size} ${this.crust} crust pizza`);
+        console.log(`baking ${this.size} ${this.crust } ${this.type} crust pizza`);
     }
 }
-const pizza = new pizza();
+const myPizza = new pizza("pastapizza");
 // new keyword is used to construct a  object from a constructor function
-pizza.bake();
+myPizza.bake();
