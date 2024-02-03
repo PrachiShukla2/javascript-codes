8c26851c9a40593d4c9c560e8273eeb7c77b21f1
PrachiShukla2 <prachishuklamount@gmail.com>
2024-02-03 22:57:40 +0530

    dom

--- jsdom/main.js
+++ jsdom/main.js
@@ -1,1 +1,13 @@
 // DOM document object model
+const view1 = document.getElementById("view1");
+console.log(view1);
+const view2  = document.querySelector("#view2");
+console.log(view2);
+view1.style.display = "flex";
+view2.style.display = "none";
+
+const views =document.getElementById("view");
+console.log(views);
+const sameViews =document.querySelectorAll(".view");
+ console.log(sameViews);
+ 
