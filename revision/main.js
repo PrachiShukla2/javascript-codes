cad3c82279f0d5226eb45aa43506cf8c7139bc57
PrachiShukla2 <prachishuklamount@gmail.com>
2024-02-10 17:33:24 +0530

    revision

---
+++ revision/main.js
@@ -0,0 +1,12 @@
+function changeText(){
+    document.getElementById("demo").innerHTML ="hola world";
+}
+   
+function batijalao(){
+    document.getElementById("image").src = "pic_bulbon.gif";
+}
+
+function batibhujao(){
+    document.getElementById("image") .src = "pic_bulboff.gif";
+}
+
