f637b8e7bc7356fe05e6d44b415a71040c01df33
PrachiShukla2 <prachishuklamount@gmail.com>
2024-02-18 20:13:23 +0530

    revise

--- revision/time.js
+++ revision/time.js
@@ -1,3 +1,19 @@
-function tellTime(){
-    document.getElementById('demo').innerHTML = date();
-}
+// function tellTime(){
+//     document.getElementById('demo').innerHTML = date();
+// }
+
+let dateObj = new Date();
+ 
+let month = String(dateObj.getMonth() + 1)
+    .padStart(2, '0');
+     
+let day = String(dateObj.getDate())
+    .padStart(2, '0');
+ 
+let year = dateObj.getFullYear();
+let output = day + '/' + month + '/' + year;
+ 
+console.log(output);
+
+// let dated = new date().todatestring();
+// console.log(dated);
