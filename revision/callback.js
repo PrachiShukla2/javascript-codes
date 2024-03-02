160d57d94a4cd0bb48a734d5b56578b34d23112c
PrachiShukla2 <prachishuklamount@gmail.com>
2024-03-02 22:46:51 +0530

    kjjh

---
+++ revision/callback.js
@@ -0,0 +1,2 @@
+// a callback function is passed as an argument to another function
+//  this alllows function to call another function a callback functoion  can run after another function has finished
