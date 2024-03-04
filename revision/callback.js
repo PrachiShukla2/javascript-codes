11bfdaad7ab57930824c3f94970d9be3fb936a1f
PrachiShukla2 <prachishuklamount@gmail.com>
2024-03-04 21:50:53 +0530

    kjk

--- revision/callback.js
+++ revision/callback.js
@@ -39,3 +39,20 @@ function meAddi(num1, num2 , callback){
 meAddi(5,5,myAdd);
 // myAdd her acts as call back function 
 // it is passed to meAddi as an argument 
+
+// removing negative numbers from the number list 
+const numbers =[4,1,-39,-98,67,-8];
+const poNum = removeNeg(numbers,(x)=>x>=0);
+// displaying result 
+document.getElementById("demo").innerHTML = poNum;
+// keeping positive numbers 
+function removeNeg(numbers,callback){
+    const array = [];
+    for(const x of numbers){
+        if (callback(x)){
+            array.push(x);
+
+        }
+    }
+    return array;
+}
