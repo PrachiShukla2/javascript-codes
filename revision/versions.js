fb44e36198ceae778d5b053aae6476a7a45277cc
PrachiShukla2 <prachishuklamount@gmail.com>
2024-03-09 17:47:00 +0530

    lkl

--- revision/versions.js
+++ revision/versions.js
@@ -1,3 +1,6 @@
 // js was invented by brendan eich in 1995 and ecma is ist real standard name 
 // ES is fully suported in all modern browsers
-// browswer
+// ES5  uses use strict directive it defines the code in the strict mode 
+// use strict is just a string expression .
+// ecmascript 2015 was the secomd major revisiomn to js 
+//   
