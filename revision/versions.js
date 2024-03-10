d0f6371489987996b70ecf1d939483b6614b1e54
PrachiShukla2 <prachishuklamount@gmail.com>
2024-03-10 22:59:04 +0530

    kjk

--- revision/versions.js
+++ revision/versions.js
@@ -3,4 +3,10 @@
 // ES5  uses use strict directive it defines the code in the strict mode 
 // use strict is just a string expression .
 // ecmascript 2015 was the secomd major revisiomn to js 
-//   
+// html is to define the comtent of web pages
+//  css to specify the layout odf the web page
+// js to program the behavior of web pages 
+//  parseInt parses a string ansd returns a whole number. spaces are allowed and only first number is returned 
+// if a number cannot be converted nan is returned 
+// parsefloat returns a float as lyk parseint returns a int 
+// they both are global methods 
